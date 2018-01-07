ba59d3356927f963bd39d6299e64acc77c76154c
Dazzla <dazzlebown@gmail.com>
2018-01-07 14:45:19 +0000

    Creates separate admin steps and defs

---
+++ behave_unchained/features/admin.feature
@@ -0,0 +1,5 @@
+Feature: Admin
+
+  Scenario: I can administer polls objects
+    When I am logged in as an administrator
+    Then I access polls admin
---
+++ behave_unchained/features/steps/admin_steps.py
@@ -0,0 +1,28 @@
+from behave import *
+from behave_unchained.features import environment
+from behave_unchained.features.lib.pages.admin_page import AdminPage
+from behave_unchained.features.lib.pages.object_admin_page import ObjectAdminPage
+from behave_unchained.features.lib import passwords
+
+use_step_matcher("re")
+
+@step("I am logged in as an administrator")
+def step_impl(context):
+    admin_page = AdminPage(context)
+    admin_page.visit(environment.ADMIN_URL)
+    admin_page.log_in(environment.ADMIN_USERNAME, passwords.ADMIN_PASSWORD)
+    assert(admin_page.is_logged_in())
+
+
+# @step("I access polls admin")
+# def step_impl(context):
+#     object_admin = ObjectAdminPage(context)
+#     object_admin.visit(environment.ADMIN_URL + 'polls')
+#     assert (object_admin.is_displayed())
+
+
+@step("I access (?P<object_type>\w+) admin")
+def step_impl(context, object_type):
+    object_admin = ObjectAdminPage(context)
+    object_admin.visit(environment.ADMIN_URL + object_type)
+    assert (object_admin.is_displayed())
